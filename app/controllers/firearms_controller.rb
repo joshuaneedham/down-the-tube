af763bb90380996cbdfa9bf902fae084f799e0cf
Joshua Needham <joshuabneedham@gmail.com>
2018-01-30 11:10:37 -0700

    Updated firearms controller

--- app/controllers/firearms_controller.rb
+++ app/controllers/firearms_controller.rb
@@ -16,4 +16,43 @@ class FirearmsController < ApplicationController
     end
   end
 
+  post '/firearms' do
+    @firearm = current_user.firearms.create(:name => params["name"], :round_count => params["round_count"])
+    redirect to "/firearms/#{@firearm.id}"
+  end
+
+  get '/firearms/:id/edit' do
+    if logged_in?
+      @firearm = Firearm.find_by_id(params[:id])
+      if @firearm.user_id == current_user.id
+        erb :'firearms/edit_firearm'
+      else
+        redirect to '/firearms'
+      end
+      redirect to '/login'
+    end
+  end
+
+  patch '/firearms/:id' do
+    @firearm = Firearm.find_by_id(params[:id])
+    @firearm.name = params[:name]
+    @firearm.round_count = params[:round_count]
+    @firearm.save
+    redirect to "/firearms/#{@firearm.id}"
+  end
+
+  delete '/firearms/:id/delete' do
+    if logged_in?
+      @firearm = Firearm.find_by_id(params[:id])
+      if @firearm.user_id == current_user.id
+        @firearm.delete
+        redirect to '/firearms'
+      else
+        redirect to '/firearms'
+      end
+    else
+      redirect to '/login'
+    end
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
     user = User.find_by(:username => params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect to '/firearms'
+      redirect to "/firearms"
     else
       redirect to '/signup'
     end
